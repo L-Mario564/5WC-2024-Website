c8d78ab30a2611e64c5e91fa3af0dd18ac713b67
L-Mario564 <ka.mario564@gmail.com>
2023-07-31 20:51:24 -0600

    Init Next.js

---
+++ .eslintrc.cjs
@@ -0,0 +1,44 @@
+/** @type {import("eslint").Linter.Config} */
+const config = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: true
+  },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'next/core-web-vitals',
+    'plugin:@typescript-eslint/recommended-type-checked',
+    'plugin:@typescript-eslint/stylistic-type-checked',
+    'prettier'
+  ],
+  rules: {
+    '@typescript-eslint/array-type': 'off',
+    '@typescript-eslint/consistent-type-definitions': 'off',
+    '@typescript-eslint/consistent-type-imports': [
+      'warn',
+      {
+        prefer: 'type-imports',
+        fixStyle: 'inline-type-imports'
+      }
+    ],
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    'quotes': [
+      'warn',
+      'single',
+      {
+        avoidEscape: true
+      }
+    ],
+    'semi': ['warn', 'always'],
+    'no-var': 'warn',
+    'brace-style': ['warn', '1tbs'],
+    'comma-dangle': ['warn', 'never'],
+    'default-case': 'error',
+    'prefer-const': 'off',
+    '@typescript-eslint/no-inferrable-types': 'off',
+    'quote-props': ['warn', 'consistent'],
+    'unused-export-let': 'off'
+  }
+};
+
+module.exports = config;
