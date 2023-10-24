08b50b0eab711fee820ae70f6adacf6edfe16408
Giovanni Martire <akinari@outlook.it>
2023-10-24 15:26:29 +0200

    MD to MDX change

--- app/rules/page.tsx
+++ app/rules/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import RulesMDX from './5WC_Rules_Full.md';
+import RulesMDX from './5WC_Rules_Full.mdx';
 import styles from './rules.module.scss';
 
 export default function RulesPage() {
