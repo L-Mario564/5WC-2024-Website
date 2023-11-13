ac4b4f1b9b30c6ae4bf9ee4a19f04ce7450c2061
Giovanni Martire <akinari@outlook.it>
2023-11-13 09:54:36 +0100

    Unused logo CSS

--- components/LandingLogo/LandingLogo.tsx
+++ components/LandingLogo/LandingLogo.tsx
@@ -1,11 +1,10 @@
 'use client';
 import logo from '@/public/landing-header.svg';
 import Image from 'next/image';
-import styles from './logo.module.scss';
 
 export default function LandingLogo() {
   return (
-    <div className={styles.logo}>
+    <div>
       <Image src={logo} alt="5WC Logo"></Image>
     </div>
   );
